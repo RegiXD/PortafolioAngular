1bc1fef4c5d3858656753555471121062059df3e
RegiXD <reitanomariaregina@gmail.com>
2023-01-25 11:03:34 -0300

    Proyectos

--- src/app/Components/proyectos/proyectos.component.ts
+++ src/app/Components/proyectos/proyectos.component.ts
@@ -6,11 +6,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./proyectos.component.css']
 })
 export class ProyectosComponent {
-  @Input() estudios = [
-    {titulo: 'Mi Primer página Web', subtitulo: 'Fue realizada únicamente con HTML y CSS', descripcion: '', fecha: '', img: './assets/Imagenes/MiPrimerPagWeb.jpg'},
+  @Input() proyectos = [
+    {titulo: 'Mi Primer página Web', descripcion: 'Fue realizada únicamente con HTML y CSS', img: './assets/Imagenes/MiPrimerPagWeb.jpg', link: '#'},
 
-    {titulo: 'UNL', subtitulo: 'Ingeniería en Informática', descripcion: 'Promosioné primer año, permitiéndome hacer el segundo en el cual dejé para dedicarme de lleno a la programación web', fecha: 'hace 1 año', img: './assets/Imagenes/UNL.png'},
+    {titulo: 'Prubas con Vue y Vuetify', descripcion: 'En esta página se encuentran diferentes secciones en las que probé diferentes componentes de Vuetify', img: './assets/Imagenes/PruebasconVuetify.jpg', link: '#'},
 
-    {titulo: 'IPET Juan XXIII', subtitulo: 'Técnica en Electrónica', descripcion: 'Terminé la escuela secundaria técnica con el segundo mejor promedio recibiendo el título de técnica el Electrónica. En dicha escuela conocí la programación, arrancando por primera vez con C y programando pic y Arduino', fecha: 'Hace 4 años', img: './assets/Imagenes/JuanXXIII.png'}
+    {titulo: 'Calculadora', descripcion: 'Proyecto de calculadora. Fue realizado como capacitación para el ingreso a una empresa Paranaense (ETsol)', img: './assets/Imagenes/Calculadora.jpg', link: '#'}
   ];
 }
